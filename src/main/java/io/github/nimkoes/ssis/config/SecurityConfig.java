e8684d0040d97191d4e3251245d120a8495610ef
nimkoes <xxxelppa@gmail.com>
2025-03-09 13:43:02 +0900

    forward, redirect example

--- src/main/java/io/github/nimkoes/ssis/config/SecurityConfig.java
+++ src/main/java/io/github/nimkoes/ssis/config/SecurityConfig.java
@@ -1,27 +1,24 @@
 package io.github.nimkoes.ssis.config;
 
+import io.github.nimkoes.ssis.filter.CustomGenericFilter;
+import io.github.nimkoes.ssis.filter.CustomOnceFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.securityMatcher("health")
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/health").permitAll()
-                                .anyRequest().authenticated()
-                );
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .addFilterAfter(new CustomGenericFilter(), LogoutFilter.class)
+                .addFilterAfter(new CustomOnceFilter(), LogoutFilter.class);
 
         return http.build();
     }
--- src/main/java/io/github/nimkoes/ssis/controller/SampleController.java
+++ src/main/java/io/github/nimkoes/ssis/controller/SampleController.java
@@ -1,13 +1,25 @@
 package io.github.nimkoes.ssis.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class SampleController {
 
-    @GetMapping("/health")
-    public String health() {
-        return "OK";
+    @GetMapping("/test-filter-before-forward")
+    public String beforeForward() {
+        return "forward:/test-filter-after";
+    }
+
+    @GetMapping("/test-filter-before-redirect")
+    public String beforeRedirect() {
+        return "redirect:/test-filter-after";
+    }
+
+    @GetMapping("/test-filter-after")
+    @ResponseBody
+    public String after() {
+        return "hello security!";
     }
 }
